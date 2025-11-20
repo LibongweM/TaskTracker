f6e74aedf10c1b7a3fb1566068ff3cbe09ba430a
Libongwe Mthonti <mthonti0z@gmail.com>
2025-11-20 19:23:25 +0200

    feature: complete sort & searching

--- task-tracker-fe/src/app/app.routes.ts
+++ task-tracker-fe/src/app/app.routes.ts
@@ -7,6 +7,7 @@ export const routes: Routes = [
     {
         component: HomePageComponent,
         path: "",
+        title: "Home",
     },
     {
         component: CreatePageComponent,
--- task-tracker-fe/src/app/pages/home-page/home-page.component.ts
+++ task-tracker-fe/src/app/pages/home-page/home-page.component.ts
@@ -1,24 +1,15 @@
 import { Component, PipeTransform, inject } from "@angular/core";
 import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { Observable, combineLatest } from "rxjs";
 import { Router, RouterLink } from "@angular/router";
 import { TASKS_DUMMY, TaskDummy } from "../../test.data";
-import { map, startWith } from "rxjs/operators";
+import { map, startWith, switchMap } from "rxjs/operators";
 
 import { AsyncPipe } from "@angular/common";
 import { NgbHighlight } from "@ng-bootstrap/ng-bootstrap";
-import { Observable } from "rxjs";
+import { Sort } from "../../enums/sort";
 import { Task } from "../../interfaces/task";
 import { TaskTrackerService } from "../../services/task-tracker.service";
-
-function search(text: string): TaskDummy[] {
-    return TASKS_DUMMY.filter((task) => {
-        const term = text.toLowerCase();
-        return (
-            task.title.toLowerCase().includes(term) ||
-            task.description.toLowerCase().includes(term)
-        );
-    });
-}
 
 @Component({
     selector: "app-home-page",
@@ -27,18 +18,32 @@ function search(text: string): TaskDummy[] {
     styleUrl: "./home-page.component.css",
 })
 export class HomePageComponent {
-    tasks$: Observable<TaskDummy[]>;
     filter = new FormControl("", { nonNullable: true });
+    sort = new FormControl(Sort.Asc, { nonNullable: true });
     private router = inject(Router);
     private taskService = inject(TaskTrackerService);
+    tasks$: Observable<Task[]>;
+    sortOptions: { label: string; value: Sort }[] = Object.keys(Sort)
+        .filter((key) => isNaN(Number(key)))
+        .map((key) => ({
+            label: key,
+            value: Sort[key as keyof typeof Sort],
+        }));
 
     constructor() {
-        // this.tasks$ = this.filter.valueChanges.pipe(
-        //     startWith(""),
-        //     map((text) => search(text))
-        // );
+        const searchTerms$ = this.filter.valueChanges.pipe(startWith(""));
+        const sortOption$ = this.sort.valueChanges.pipe(startWith(Sort.Asc));
 
-        this.tasks$ = this.taskService.getAllTasks({});
+        this.tasks$ = combineLatest([searchTerms$, sortOption$]).pipe(
+            // Use switchMap only when the search term changes (API call)
+            switchMap(([searchTerm, sortOption]) =>
+                this.search(searchTerm, sortOption)
+            )
+        );
+    }
+
+    search(text: string, sort: Sort): Observable<Task[]> {
+        return this.taskService.getAllTasks({ searchTerm: text, sort });
     }
 
     view(id: number) {
